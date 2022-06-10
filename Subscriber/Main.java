3e0804a206e7fe8cbb8e1aee947c96e1a2bbd3b9
EugeneHH <zenadegtarev766@gmail.com>
2022-06-10 11:42:20 +0100

    student

---
+++ Subscriber/Main.java
@@ -0,0 +1,18 @@
+package Subscriber;
+
+public class Main {
+    public static void main(String[] args) {
+         Subscriber [] subArray = new Subscriber[10];
+
+        subArray [1] = new Subscriber("1", "Lobanov", "Andrey", "Olegovich", "Kyiv", "380954476588", "#12", "3500", "1500", "∞");
+        subArray [2] = new Subscriber("2", "Okun", "Vlad", "Valerievich", "Dnipro", "380930903242", "#25", "2500", "1000", "10000mb");
+        subArray [3] = new Subscriber("3", "Ermolaev", "Valeriy", "Nickolaevich", "Nickolaev", "380675672309", "#3", "1600", "400", "3500mb");
+        subArray [4] = new Subscriber("4", "Cherishuk", "Ekaterina", "Dmitrievna", "Nickolaev", "380687080522", "#12", "3500", "1500", "∞");
+        subArray [5] = new Subscriber("5", "Uhanova", "Alina", "Sergeevna", "Kyiv", "380987080834", "#4", "3000", "1200", "∞");
+        subArray [6] = new Subscriber("6", "Chaban", "Nina", "Yourievna", "Odessa", "380507391862", "#25", "2500", "1000", "10000mb");
+        subArray [7] = new Subscriber("7", "Semenyouk", "Alexsandr", "Petrivich", "Odessa", "380938759081", "#3", "1600", "400", "3500mb");
+        subArray [8] = new Subscriber("8", "Malina", "Artem", "Penasovich", "Lviv", "380433035644", "#12", "3500", "1500", "∞");
+        subArray [9] = new Subscriber("9", "Senko", "Ivan", "Ivanivich", "Dnipro", "380637834599", "#4", "3000", "1200", "∞");
+        subArray [10] = new Subscriber("10", "Maxno", "Alisa", "Batikovna", "Kharkiv", "380977472589", "#13", "4000", "2000", "∞");
+    }
+}
